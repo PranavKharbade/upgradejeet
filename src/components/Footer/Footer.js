11a5fe62d63c6e3e271d8b5eec07870595b67bfe
PranavKharbade <36626093+PranavKharbade@users.noreply.github.com>
2021-08-24 16:11:58 +0530

    Add files via upload
    initial upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './style.css';
+
+export class Footer extends Component {
+  render() {
+    return (
+      <footer>Hi, I am footer@pranav </footer>
+    )
+  }
+}
+
+export default Footer;
